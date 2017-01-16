8b1ca850946cb3796ec41d19f297c2b5a80e1845
Michael Odere <michael@odereinc.com>
2017-01-15 16:16:31 -0800

    final

--- install_daemons.sh
+++ install_daemons.sh
@@ -7,4 +7,16 @@ git clone -b $branch $repository
 cd postgres
 ./configure prefix=$1
 make
-install
+make install
+
+cd ..
+
+# Download apache
+link=http://www-us.apache.org/dist//httpd/httpd-2.4.25.tar.gz
+curl -o apache-2-4-25.tar.gz $link
+echo "Downloading apache from $link version of postgres"
+tar xzf apache-2-4-25.tar.gz
+cd apache-2-4-25
+./configure prefix=$1
+make
+make install
