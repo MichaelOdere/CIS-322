190143b7cc505ced88ba06d7337ecff29e483bda
Michael Odere <michael@odereinc.com>
2017-01-16 06:14:49 +0000

    minor fixs

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,96 +1,96 @@
 CREATE TABLE products(
-	product_pk integer,
-	vendor varchar(128) not null,
+	product_pk serial primary key,
+	vendor varchar(16) not null,
 	description varchar(128) not null,
 	alt_description varchar(128) not null
 );
 
 CREATE TABLE assets(
-	asset_pk integer,
-	product_fk integer,
-	asset_tag varchar(128) not null,
-	description varchar(128) not null,
-	alt_description varchar(128) not null
+	asset_pk serial primary key,
+	product_fk integer REFERENCES products(product_pk) not null,
+	asset_tag varchar(16) not null,
+	description varchar(16) not null,
+	alt_description varchar(16) not null
 );
 
 CREATE TABLE vehicles(
-	vehicle_pk integer,
-	asset_fk integer
+	vehicle_pk serial primary key,
+	asset_fk integer REFERENCES assets(asset_pk) not null
 );
 
 CREATE TABLE facilities(
-	facility_pk integer,
-	fcode varchar(128) not null,
-	common_name varchar(128) not null,
-	location varchar(128) not null
+	facility_pk serial primary key,
+	fcode varchar(16) not null,
+	common_name varchar(16) not null,
+	location varchar(16) not null
 );
 
 CREATE TABLE asset_at(
-	asset_fk integer,
-	facility_fk integer,
+	asset_fk integer REFERENCES assets(asset_pk) not null,
+	facility_fk integer REFERENCES facilities(facility_pk) not null,
 	arrive_dt timestamp,
 	depart_dt timestamp
 );
 
 CREATE TABLE convoys(
-	convoy_py integer,
-	request varchar(128),
-	source_fk integer,
-	dest_fk integer,
+	convoy_pk  serial primary key,
+	request varchar(16),
+	source_fk integer REFERENCES facilities(facility_pk) not null,
+	dest_fk integer REFERENCES facilities(facility_pk) not null,
 	depart_dt timestamp,
 	arrive_dt timestamp
 );
 
 CREATE TABLE used_by(
-	vehicle_fk integer,
+	vehicle_fk integer REFERENCES vehicles(vehicle_pk) not null,
 	convok_fk integer
 );
 
 CREATE TABLE asset_on(
-	asset_fk integer,
-	convoy_fk integer,
+	asset_fk integer  REFERENCES assets(asset_pk) not null,
+	convoy_fk integer REFERENCES convoys(convoy_pk) not null,
 	load_dt timestamp,
 	unload_dt timestamp
 );
 
 CREATE TABLE users(
-	user_pk integer,
+	user_pk  serial primary key,
 	username varchar(128),
 	active boolean
 );
 
 CREATE TABLE roles(
-	role_pk integer,
-	title varchar(128)
+	role_pk serial primary key,
+	title varchar(16)
 );
 
 CREATE TABLE user_is(
-	user_fk integer,
-	role_fk integer
+	user_fk integer REFERENCES users(user_pk) not null,
+	role_fk integer REFERENCES roles(role_pk) not null
 );
 
 CREATE TABLE user_supports(
-	user_fk integer,
-	facility_fk integer
+	user_fk integer REFERENCES users(user_pk) not null,
+	facility_fk integer REFERENCES facilities(facility_pk) not null
 );
 
 CREATE TABLE levels(
-	level_pk integer,
+	level_pk serial primary key,
 	abbrv varchar(128),
 	comment varchar(128)
 );
 
 CREATE TABLE compartments(
-	compartment_pk integer,
+	compartment_pk serial primary key,
 	abbrv varchar(128),
 	comment varchar(128)
 );
 
 CREATE TABLE security_tags(
-	tag_pk integer,
-	level_fk integer,
-	compatment_fk integer,
-	user_fk integer,
-	product_fk integer,
-	asset_fk integer
+	tag_pk serial primary key,
+	level_fk integer REFERENCES levels(level_pk) not null,
+	compartment_fk integer REFERENCES compartments(compartment_pk) not null, 
+	user_fk integer REFERENCES users(user_pk) not null,
+	product_fk integer REFERENCES products(product_pk) not null,
+	asset_fk integer REFERENCES assets(asset_pk) not null
 );
