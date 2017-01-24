9fb52ae2d5ec2a6479680d8bc46336558f90cf6f
Michael Odere <michael@odereinc.com>
2017-01-24 07:15:24 +0000

    Final script

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -3,4 +3,7 @@
 link=https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
 curl -O $link
 tar xzf osnap_legacy.tar.gz
-
+tar xzf osnap_legacy.tar.gz
+python product_insert.py $1 $2
+rm osnap_legacy.tar.gz
+rm -r osnap_legacy
