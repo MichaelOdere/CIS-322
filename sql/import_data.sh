aea6e94341e7e4650aeadd19c2a00c009974c4e9
Michael Odere <michael@odereinc.com>
2017-02-13 05:49:41 +0000

    delete

--- sql/import_data.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-link=https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz
-curl -O $link
-tar xzf osnap_legacy.tar.gz
-tar xzf osnap_legacy.tar.gz
-python product_insert.py $1 $2
-rm osnap_legacy.tar.gz
-rm -r osnap_legacy
